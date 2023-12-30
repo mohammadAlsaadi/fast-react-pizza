b1a071bfa33cda4a69513f94bbbfbc2a6567b710
mohammad alsaadi <moalsaadiformal@gmail.com>
2023-12-30 14:53:51 +0300

    add tailwind css to the project

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20  px-3  py-3 backdrop-blur-sm">
+      <div className="loader    "></div>
+    </div>
+  );
+}
+
+export default Loader;
